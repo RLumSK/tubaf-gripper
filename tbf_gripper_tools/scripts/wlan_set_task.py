ea59f23241781075eabc92c0f1e06ca03590a124
Steve Grehl <Steve.Grehl@informatik.tu-freiberg.de>
2016-10-28 13:48:22 +0200

    added new abstract class hierachie and wlan set station task

--- tbf_gripper_tools/scripts/wlan_set_task.py
+++ tbf_gripper_tools/scripts/wlan_set_task.py
@@ -57,10 +57,18 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 
-class WaterSampleTask(autonomy.Task.SetTask):
+class WlanSetTask(autonomy.Task.SetTask):
     """
-    Class to get a water sample
+    Class to get a set a wlan station
     """
+    def __init__(self, station_nr=1):
+        """
+        default constructor of the set wlan task
+        """
+        autonomy.Task.SetTask.__init__(self)
+        self.waypoints = rospy.get_param("~waypoints_station_"+str(station_nr))
+        self.exec_thread = None
+
 
     def perform(self):
         """
@@ -68,58 +76,54 @@ class WaterSampleTask(autonomy.Task.SetTask):
         :return: -
         :rtype: -
         """
-        rospy.loginfo("WaterSampleTask.perform(): Move to Station on top of the Robot starting at HOME position")
+        rospy.loginfo("WlanSetTask.perform(): Move to Station on top of the Robot starting at HOME position")
         # Move to Station on top of the Robot starting at HOME position
         self.move_wait(self.waypoints["home_pose"], v=self.j_arm_speed, a=self.j_arm_acceleration, move_cmd="movej")
         self.hand_controller.openHand()
-        self.move_wait(self.waypoints["pre_pickup"], v=self.j_arm_speed, a=self.j_arm_acceleration, move_cmd="movej")
-        self.move_wait(self.waypoints["pickup"], v=self.j_arm_speed, a=self.j_arm_acceleration, move_cmd="movej")
+        self.move_wait(self.waypoints["pre_grasp"], v=self.j_arm_speed, a=self.j_arm_acceleration, move_cmd="movej")
+        self.move_wait(self.waypoints["grasp"], v=self.l_arm_speed, a=self.l_arm_acceleration, move_cmd="movel")
 
-        rospy.loginfo("WaterSampleTask.perform(): Grasp station")
+        rospy.loginfo("WlanSetTask.perform(): Grasp station")
         # Grasp station
         self.hand_controller.closeHand()
         rospy.sleep(5.)
-        self.move_wait(self.waypoints["lift"], v=self.l_arm_speed, a=self.l_arm_acceleration, move_cmd="movel")
-        self.move_wait(self.waypoints["post_pickup"], v=self.j_arm_speed, a=self.j_arm_acceleration, move_cmd="movej")
+        self.move_wait(self.waypoints["post_grasp"], v=self.l_arm_speed, a=self.l_arm_acceleration, move_cmd="movel")
 
         # Set station
-        rospy.loginfo("WaterSampleTask.perform(): Set station")
-        self.move_wait(self.waypoints["hover"], v=self.j_arm_speed, a=self.j_arm_acceleration, move_cmd="movej")
-        self.move_wait(self.waypoints["sample_pose"], v=self.l_arm_speed, a=self.l_arm_acceleration, move_cmd="movel")
+        rospy.loginfo("WlanSetTask.perform(): Set station")
+        self.move_wait(self.waypoints["pre_set"], v=self.j_arm_speed, a=self.j_arm_acceleration, move_cmd="movej")
+        self.move_wait(self.waypoints["set"], v=self.l_arm_speed, a=self.l_arm_acceleration, move_cmd="movel")
 
-        rospy.loginfo("WaterSampleTask.perform(): Take sample -automatic-")
-        # Take sample -automatic-
-        rospy.sleep(30.)
-
-        rospy.loginfo("WaterSampleTask.perform(): Lift station")
-        # Lift station
-        self.move_wait(self.waypoints["lifted"], v=self.l_arm_speed, a=self.l_arm_acceleration, move_cmd="movel")
-
-        # TODO: check if successful
-
-        rospy.loginfo("WaterSampleTask.perform(): Move to origin")
-        # Move to origin
-        self.move_wait(self.waypoints["post_pickup"], v=self.j_arm_speed, a=self.j_arm_acceleration, move_cmd="movej")
-        self.move_wait(self.waypoints["lift"], v=self.j_arm_speed, a=self.j_arm_acceleration, move_cmd="movej")
-        self.move_wait(self.waypoints["pickup"], v=self.l_arm_speed, a=self.l_arm_acceleration, move_cmd="movel")
-
-        rospy.loginfo("WaterSampleTask.perform(): Release station")
-        # Release station
+        rospy.loginfo("WlanSetTask.perform(): Release station")
         self.hand_controller.openHand()
         rospy.sleep(5.)
 
-        rospy.loginfo("WaterSampleTask.perform(): Move back to home station")
+        rospy.loginfo("WlanSetTask.perform(): Return to home pose")
         # Move back to home station
-        self.move_wait(self.waypoints["lift"], v=self.l_arm_speed, a=self.l_arm_acceleration, move_cmd="movel")
-        self.move_wait(self.waypoints["pre_pickup"], v=self.j_arm_speed, a=self.j_arm_acceleration, move_cmd="movej")
+        self.move_wait(self.waypoints["post_set"], v=self.l_arm_speed, a=self.l_arm_acceleration, move_cmd="movel")
+        self.move_wait(self.waypoints["pre_set"], v=self.l_arm_speed, a=self.l_arm_acceleration, move_cmd="movel")
         self.move_wait(self.waypoints["home_pose"], v=self.j_arm_speed, a=self.j_arm_acceleration, move_cmd="movej")
-        self.hand_controller.closeHand()
+
         self.exec_thread = None
 
+
+    def start(self):
+        """
+        Start the water Sample Task
+        :return: -
+        :rtype: -
+        """
+        rospy.loginfo("SetTask.start():")
+        self.run_as_process(WlanSetTask.perform)
+
 if __name__ == '__main__':
-    rospy.init_node("WaterSampleTask")
-    obj = WaterSampleTask()
-    while obj.exec_thread is not None:
-        rospy.sleep(0.5)
-    obj.start()
+    rospy.init_node("WlanSetTask")
+    obj = None
+    for i in range(1, 3):
+        if obj is None or obj.exec_thread is None:
+            obj = WlanSetTask(i)
+            rospy.loginfo("wlan_set_task.py: main(): Start station: %d", i)
+            obj.start()
+        while obj.exec_thread is not None:
+            rospy.sleep(0.5)
     rospy.spin()
