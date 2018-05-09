b1cb3d16db4f75236b180e77c7fd063f56e07c85
Steve Grehl <Steve.Grehl@informatik.tu-freiberg.de>
2018-05-09 14:48:00 +0200

    WIP implemented movit interface into wlan set task - not tested or runned just implementation

--- tbf_gripper_tools/scripts/wlan_set_task.py
+++ tbf_gripper_tools/scripts/wlan_set_task.py
@@ -31,8 +31,11 @@
 
 import signal
 import sys
+import os
 
-import rospy
+import rospy, rospkg
+import moveit_commander, moveit_msgs.msg, geometry_msgs.msg, geometry_msgs.msg, std_msgs.msg
+import message_filters
 
 import autonomy.Task
 import numpy as np
@@ -69,9 +72,37 @@ class WlanSetTask(autonomy.Task.SetTask):
         self.waypoints = rospy.get_param("~waypoints_station_"+str(station_nr))
         self.exec_thread = None
 
+        # Initialize MoveIt!
+        # see: https://github.com/ros-planning/moveit_tutorials/blob/indigo-devel/doc/pr2_tutorials/planning/scripts/move_group_python_interface_tutorial.py
+        moveit_commander.roscpp_initialize(sys.argv)
+        self.mvit_robot = moveit_commander.RobotCommander()
+        self.mvit_scene = moveit_commander.PlanningSceneInterface()
+        self.mvit_group = moveit_commander.MoveGroupCommander("UR5")
+        self.display_trajectory_publisher = rospy.Publisher("/move_group/display_planned_path",
+                                                            moveit_msgs.msg.DisplayTrajectory)
+        rospy.sleep(10)
+
+        #  Add collision object to planning scene
+        self.eef_link = rospy.get_param("~eef_link", "gripper_robotiq_planning")
+        ssb_default_pose = rospy.get_param("~ssb_default_pose", geometry_msgs.msg.Pose())
+        ssb_mesh_filename = rospy.get_param("~ssb_mesh_filename", os.path.join(
+            rospkg.RosPack().get_path('tbf_gripper_tools'), 'resources', 'mesh', 'wlan_box.stl'))
+        x_scale = rospy.get_param("~ssb_x_scale", 1.0)
+        y_scale = rospy.get_param("~ssb_y_scale", 1.0)
+        z_scale = rospy.get_param("~ssb_z_scale", 1.0)
+        self.ssb_name = "Smart_Sensor_Box"
+        self.mvit_scene.add_mesh(name=self.ssb_name, pose=ssb_default_pose, filename=ssb_mesh_filename,
+                                 size=(x_scale, y_scale, z_scale))
+
+        ui_pose_topic = rospy.get_param("~ui_pose_topic", "/wlan_set_task/ui_pose")
+        self.ui_pose_subscriber = message_filters.Subscriber(ui_pose_topic, geometry_msgs.msg.Pose)
+        self.ui_pose_cache = message_filters.Cache(self.ui_pose_subscriber, 5)
+        self.ui_check_subscriber = message_filters.Subscriber(ui_pose_topic, std_msgs.msg.Bool)
+        self.ui_check_cache = message_filters.Cache(self.ui_check_subscriber, 5)
+
     def perform(self):
         """
-        Hard coded task to pickup a water sample station from the robot, take a sample and return it to the robot
+        Hard coded task to pickup a wlan box from the robot, set it on the ground and the return to a home pose
         :return: -
         :rtype: -
         """
@@ -87,23 +118,51 @@ class WlanSetTask(autonomy.Task.SetTask):
         # Grasp station
         self.hand_controller.closeHand()
         rospy.sleep(5.)
+        # Attach collision object to end effector
+        self.mvit_scene.attach_mesh(link=self.eef_link, name=self.ssb_name)
         self.move_wait(self.waypoints["post_grasp"], v=self.l_arm_speed, a=self.l_arm_acceleration, move_cmd="movel")
 
         # Set station
         rospy.loginfo("WlanSetTask.perform(): Set station")
-        self.move_wait(self.waypoints["pre_set"], v=self.j_arm_speed, a=self.j_arm_acceleration, move_cmd="movej")
-        self.move_wait(self.waypoints["set"], v=self.l_arm_speed, a=self.l_arm_acceleration, move_cmd="movel")
+        # Query Goal from User Interface
+        ui_pose = self.ui_pose_cache.getElemAfterTime(rospy.Time.now())
+        # Plan Path using Moveit
+        self.mvit_group.clear_pose_targets()
+        self.mvit_group.set_pose_target(pose=ui_pose, end_effector_link=self.eef_link)
+
+        plan_valid = False
+        while not plan_valid:
+            rospy.loginfo("WlanSetTask.perform(): Planning ...")
+            plan = self.mvit_group.plan()
+            # Check Plan
+            now = rospy.Time.now()
+            rospy.sleep(5.0)
+            plan_valid = self.ui_check_cache.getElemAfterTime(now)
+        rospy.loginfo("WlanSetTask.perform(): Executing ...")
+        self.mvit_group.execute(plan)
 
         rospy.loginfo("WlanSetTask.perform(): Release station")
         self.hand_controller.openHand()
         rospy.sleep(5.)
+        # Remove collision object from end effector
+        self.mvit_scene.remove_attached_object(link=self.eef_link, name=self.ssb_name)
         # self.move_wait(self.waypoints["set_down"], v=self.j_arm_speed, a=self.j_arm_acceleration, move_cmd="movej")
 
         rospy.loginfo("WlanSetTask.perform(): Return to home pose")
-        # Move back to home station
-        self.move_wait(self.waypoints["post_set"], v=self.l_arm_speed, a=self.l_arm_acceleration, move_cmd="movel")
-        self.move_wait(self.waypoints["pre_set"], v=self.l_arm_speed, a=self.l_arm_acceleration, move_cmd="movel")
-        self.move_wait(self.waypoints["home_pose"], v=self.j_arm_speed, a=self.j_arm_acceleration, move_cmd="movej")
+        # Plan back to home station
+        self.mvit_group.clear_pose_targets()
+        self.mvit_group.set_joint_value_target(self.waypoints["home_pose"])
+
+        plan_valid = False
+        while not plan_valid:
+            rospy.loginfo("WlanSetTask.perform(): Planning ...")
+            plan = self.mvit_group.plan()
+            # Check Plan
+            now = rospy.Time.now()
+            rospy.sleep(5.0)
+            plan_valid = self.ui_check_cache.getElemAfterTime(now)
+        rospy.loginfo("WlanSetTask.perform(): Executing ...")
+        self.mvit_group.execute(plan)
 
         self.exec_thread = None
 
