4b6e9105e499d90f703f008e703b15ededb3724e
grehl <Steve.Grehl@informatik.tu-freiberg.de>
2020-02-20 14:15:35 +0100

    moved docker to meta package since build stuff

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,7 @@
 source "/opt/ros/kinetic/setup.bash"
 source "/julius_ws/devel/setup.bash"
 
-exec "$@"
+roscore &
+rosrun tbf_gripper_autonomy evaluate_bag.py _plot_dir:="/out/plots" _bag:="/in/bag/$1"
+
+#exec "$@"
