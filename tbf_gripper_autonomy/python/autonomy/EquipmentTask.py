f4ecf562f920a671fee992999e189ab85c903271
Steve Grehl <Steve.Grehl@informatik.tu-freiberg.de>
2020-06-30 15:35:15 +0200

    optimize_ssb_z_rotation conform with thesis

--- tbf_gripper_autonomy/python/autonomy/EquipmentTask.py
+++ tbf_gripper_autonomy/python/autonomy/EquipmentTask.py
@@ -123,138 +123,7 @@ def sense(tf_listener=None):
     return ret_ps
 
 
-def adjust_z_rotation(ps_object):
-    """
-    Adjust the z-Rotation of the given pose in order to be easier to reach for the robot
-    :param ps_object: pose to adjust
-    :type ps_object: PoseStamped
-    :return: adjusted pose
-    :rtype: PoseStamped
-    """
-    ret_ps = PoseStamped()
-    ret_ps.header = ps_object.header
-
-    from scipy.spatial.transform import Rotation
-    rotation = Rotation.from_dcm(pose_to_array(ps_object.pose)[:3, :3])  # type: R
-    old = rotation.as_euler('XYZ')
-    new = np.copy(old)
-    rospy.logdebug("adjust_z_rotation(): alpha %s" % old[0])
-    rospy.logdebug("adjust_z_rotation(): beta %s" % old[1])
-    rospy.logdebug("adjust_z_rotation(): gamma %s" % old[2])
-
-    # Formula
-    # frac = (2+np.cos(beta))/(1+np.cos(beta))
-    # rospy.loginfo("adjust_z_rotation(): arccos(%s)" % frac)
-    # new[2] = old[0] #new[2] - np.pi/2.0
-
-    # Compute change
-    d = new - old
-    for i in range(0, 2):
-        if d[i] > np.pi:
-            d[i] -= 2 * np.pi
-        if d[i] < -np.pi:
-            d[i] += 2 * np.pi
-
-    ret_ps.pose = rotate_pose(ps_object.pose, d)
-    rospy.logdebug("adjust_z_rotation(): dalpha %s" % d[0])
-    rospy.logdebug("adjust_z_rotation(): dbeta %s" % d[1])
-    rospy.logdebug("adjust_z_rotation(): dgamma %s" % d[2])
-    return ret_ps
-
-
 def optimize_ssb_z_rotation(oTs, sTg):
-    """
-    Calculate the missing z-Rotation of transformation T from O->S, given the transformation and S->G
-    O ... base coordinate frame, eg. base_link
-    S ... SSB coordinate frame
-    G ... gripper coordinate frame
-    The rotation among the z-axis for oTs is variable.
-    Therefore we want to minimize otg, this yields to:
-    ots = -oRs*stg
-    t ... translation
-    R ... rotation matrix
-    :param oTs: transformation from O->S as affine transformation
-    :type oTs: numpy.ndarray (4x4)
-    :param sTg: transformation from S->G as affine transformation
-    :type sTg: numpy.ndarray (4x4)
-    :return: transformation from O->S
-    :rtype: numpy.ndarray (4x4)
-    """
-    # Setup variables
-    ots = oTs[:3, 3]
-    stg = sTg[:3, 3]
-    # oRs = Rz*Ry*Rx
-    from scipy.spatial.transform import Rotation
-    R = Rotation.from_dcm(oTs[:3, :3])
-    alpha, beta, gamma = R.as_euler('xyz')
-    # Extrinsic rotation using euler angles
-    Rx = np.asarray([[1, 0, 0],
-                     [0, np.cos(alpha), np.sin(alpha)],
-                     [0, -np.sin(alpha), np.cos(alpha)]])
-    Ry = np.asarray([[np.cos(beta), 0, np.sin(beta)],
-                     [0, 1, 0],
-                     [-np.sin(beta), 0, np.cos(beta)]])
-    # 1st compute the right side (known values)
-    # Rx, Ry are orthogonal -> R'=R^-1 (AB)'=B'A'
-    B = (-1.0 / np.linalg.norm(stg)) * np.dot(ots, stg.transpose()) * np.matmul(Ry, Rx).transpose()
-
-    # 2nd extract gamma from B and calculate Rz
-    b_alpha, b_beta, b_gamma = Rotation.from_dcm(B).as_euler('xyz')
-    if b_gamma > 0:
-        b_gamma -= np.pi
-    else:
-        b_gamma += np.pi
-    Rz = np.asarray([[np.cos(b_gamma), np.sin(b_gamma), 0],
-                     [-np.sin(b_gamma), np.cos(b_gamma), 0],
-                     [0, 0, 1]])
-
-    rospy.logdebug("[optimize_ssb_z_rotation()] B \n%s\ndet(B)=%s\tgamma=%s" % (
-        B, np.linalg.det(B), Rotation.from_dcm(B).as_euler('xyz')[2]))
-    rospy.logdebug("[optimize_ssb_z_rotation()] Rz \n%s\ndet(Rz)=%s\tgamma=%s" % (
-        Rz, np.linalg.det(Rz), Rotation.from_dcm(Rz).as_euler('xyz')[2]))
-    oTs[:3, :3] = np.matmul(Rz, np.matmul(Rx, Ry))
-    # oTs[:3, :3] = np.matmul(B, np.matmul(Rx, Ry))
-    rospy.logdebug("[optimize_ssb_z_rotation()] new oTs\n%s" % oTs)
-    return oTs
-
-
-def optimize_ssb_z_rotation2(oTs, sTg):
-    """
-    Calculate the missing z-Rotation of transformation T from O->S, given the transformation and S->G
-    O ... base coordinate frame, eg. base_link
-    S ... SSB coordinate frame
-    G ... gripper coordinate frame
-    The rotation among the z-axis for oTs is variable.
-    Therefore we want to minimize otg, this yields to:
-    ots = -oRs*stg
-    t ... translation
-    R ... rotation matrix
-    :param oTs: transformation from O->S as affine transformation
-    :type oTs: numpy.ndarray (4x4)
-    :param sTg: transformation from S->G as affine transformation
-    :type sTg: numpy.ndarray (4x4)
-    :return: transformation from O->S
-    :rtype: numpy.ndarray (4x4)
-    """
-    # Setup variables
-    from scipy.spatial.transform import Rotation
-    R = Rotation.from_dcm(oTs[:3, :3])
-    alpha, beta, gamma = R.as_euler('xyz')
-    sa = np.sin(alpha)
-    ca = np.cos(alpha)
-    cb = np.cos(beta)
-
-    a_s = -sa * (1 + cb)
-    a_c = ca * (1 - cb)
-
-    gamma = np.arccos((1 - cb) / np.sqrt(a_s ** 2 + a_c ** 2)) - np.arctan(-a_s / a_c)
-    rospy.logdebug("[optimize_ssb_z_rotation()] gamma = %s°" % np.rad2deg(gamma))
-    n_wRs = Rotation.from_euler('XYZ', [alpha, beta, gamma], degrees=False)
-    oTs[:3, :3] = n_wRs.as_dcm()
-    return oTs
-
-
-def optimize_ssb_z_rotation3(oTs, sTg):
     """
     Calculate the missing z-Rotation of transformation T from O->S, given the transformation and S->G
     O ... base coordinate frame, eg. base_link
@@ -297,45 +166,25 @@ def optimize_ssb_z_rotation3(oTs, sTg):
         qy = aps[1]
         qz = aps[2]
 
-        r00 = ca * cg - sa * cb * sg
-        r01 = -ca * sg - sa * cb * cg
-        r02 = sa * sb
-        r10 = sa * cg + ca * cb * sg
-        r11 = -sa * sg + ca * cb * cg
-        r12 = -ca * sb
-        r20 = sb * sg
-        r21 = sb * cg
-        r22 = cb
+        x = (ca*cg-sa*cb*sg)*px+(-ca*sg-sa*cb*cg)*py+sa*sb*pz+qx
+        y = (sa*cg+ca*cb*sg)*px+(-sa*sg+ca*cb*cg)*py-ca*sb*pz+qy
+        z = sb*sg*px-sb*cg*py+cb*pz+qz
 
-        dr00 = -ca * sg - sa * cb * cg
-        dr01 = -ca * cg + sa * cb * sg
-        dr10 = -sa * sg + ca * cb * cg
-        dr11 = -sa * cg - ca * cb * sg
-        dr20 = sb * cg
-        dr21 = -sb * sg
+        dx = (-ca*sg-sa*cb*cg)*px+(-ca*cg+sa*cb*sg)*py
+        dy = (-sa*sg+ca*cb*cg)*px+(-sa*cg-ca*cb*sg)*py
+        dz = sb*cg*px+sb*sg*py
 
-        ddr00 = -ca * cg - sa * cb * sg
-        ddr01 = ca * sg + sa * cb * cg
-        ddr10 = -sa * cg - ca * cb * sg
-        ddr11 = sa * sg - ca * cb * cg
-        ddr20 = -sb * sg
-        ddr21 = -sb * cg
+        ddx = (-ca*cg+sa*cb*sg)*px+(ca*sg-sa*cb*cg)*py
+        ddy = (-sa*cg-ca*cb*sg)*px+(sa*sg-ca*cb*cg)*py
+        ddz = -sb*sg*px+sb*cg*py
 
-        x = r00 * px + r01 * py + r02 * pz + qx
-        dx = dr00 * px + dr01 * py
-        ddx = ddr00 * px + ddr01 * py
+        v = x**2+y**2+z**2
+        dv = 2*(x*dx+y*dy+z*dz)
+        ddv = 2*(dx**2+x*ddx+dy**2+y*ddy+dz**2+z*ddz)
 
-        y = r10 * px + r11 * py + r12 * pz + qy
-        dy = dr10 * px + dr11 * py
-        ddy = ddr10 * px + ddr11 * py
-
-        z = r20 * px + r21 * py + r22 * pz + qz
-        dz = dr20 * px + dr21 * py
-        ddz = ddr20 * px + ddr21 * py
-
-        f = np.sqrt(x ** 2 + y ** 2 + z ** 2)
-        df = (x * dx + y * dy + z * dz) / f
-        ddf = ((dx ** 2 + x * ddx + dy ** 2 + y * ddy + dz ** 2 + z * ddz) - df ** 2) / (f)
+        f = np.sqrt(v)
+        df = dv/(2.*f)
+        ddf = ((ddv/2.)-df**2)/f
 
         return [f, df, ddf]
 
@@ -515,7 +364,7 @@ class EquipmentTask(GraspTask):
             sTg = pose_to_array(self.selected_equipment.get_grasp_pose(object_pose_stamped=self.selected_equipment.ps,
                                                                        tf_listener=self.tf_listener, save_relation=True,
                                                                        use_relation=False).pose)
-            n_aTs = optimize_ssb_z_rotation3(oTs=aTs, sTg=sTg)
+            n_aTs = optimize_ssb_z_rotation(oTs=aTs, sTg=sTg)
             # rospy.logdebug("EquipmentTask.perform(3): Calculating gripper Rotation")
             # rospy.logdebug("EquipmentTask.perform(3): aTs\n%s" % aTs)
             # rospy.logdebug("EquipmentTask.perform(3): sTg\n%s" % sTg)
